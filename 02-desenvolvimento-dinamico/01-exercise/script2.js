b56fb38a83a20687a07ee9c102ca6d3f61989178
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-11 19:17:54 -0300

    add nova pergunta

--- 02-desenvolvimento-dinamico/01-exercise/script2.js
+++ 02-desenvolvimento-dinamico/01-exercise/script2.js
@@ -1,4 +1,6 @@
 let readlineSync = require('readline-sync');
 let nome = readlineSync.question('Qual o seu nome?\n');
+let idade = readlineSync.question('Qual a sua idade?\n');
 
 console.log(`Olá, ${nome}.`);
+console.log(`Sua idade é ${idade}.`);
