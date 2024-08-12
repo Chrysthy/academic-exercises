7d9d15964496a125760e3a70e0bc73ef7a674a37
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-12 19:48:51 -0300

    add new var exemple

--- 02-desenvolvimento-dinamico/02-exercise/index.js
+++ 02-desenvolvimento-dinamico/02-exercise/index.js
@@ -5,3 +5,18 @@ var b = 20;
 var total = a + b;
 
 console.log(total);
+
+
+var nomePessoa = 'Chrystine';
+console.log(nomePessoa);
+
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+
+console.log(var3);
+
+var variavel1;
+variavel1 = 23;
+
+console.log(variavel1);
