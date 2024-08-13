8588b321f9f1cf06303f357d155e1bb80565138c
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-08-13 15:51:09 -0300

    calcular media de um array

--- 02-desenvolvimento-dinamico/05-exercise/index.js
+++ 02-desenvolvimento-dinamico/05-exercise/index.js
@@ -1,1 +1,32 @@
 //arrays
+var valores = [8, 1, 7, 2, 9];
+
+console.log(valores[0]);
+
+//INTERAÇÃO DOS ELEMENTOS 
+for (var i=0; i < valores.length; i++){
+
+    console.log(`Posição ${i} - Valor ${valores[i]}`);
+
+}
+
+var carros = [];
+carros[0] = "Volvo";
+carros[1] = "Jeep";
+
+
+var motos = new Array("BMW", "Yamaha", "Honda");
+
+
+//calcular a média de todos os números de um array
+var soma = 0;
+
+for (var i=0; i < valores.length; i++){
+
+    soma += valores[i]
+
+}
+
+var media = soma/valores.length;
+
+console.log(media);
