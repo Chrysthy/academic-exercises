f2860f6f822c56059103763b6f341feea03af759
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-08-13 14:14:21 -0300

    add examples of operators

--- 02-desenvolvimento-dinamico/04-exercise/index.js
+++ 02-desenvolvimento-dinamico/04-exercise/index.js
@@ -20,3 +20,18 @@ c = 60;
 console.log(n <= x && b != c);
 console.log(n <= x || b != c);
 console.log(!(n <= x || b != c));
+
+//verficar se a é maior que b e b é divisível por 2
+console.log(a > b && b % 2 == 0);
+
+
+//verificar se b é maior igual a  ou b dividido por 2 o resultado é 2
+console.log(b >= a || b / 2 == 2);
+
+
+//verificar se a é maior que b e b maior igual a 5
+console.log(a > b && b >= 5);
+
+
+//verificar se b é par ou ímpar, usando ternário
+resultado = b % 2 == 0 ? "Par" : "Impar";
