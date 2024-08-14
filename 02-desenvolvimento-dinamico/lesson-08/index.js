54cf8135f3515c9adc4a0f2aa7d7a25d3d211af2
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-14 20:35:47 -0300

    else, else if e else

---
+++ 02-desenvolvimento-dinamico/lesson-08/index.js
@@ -0,0 +1,22 @@
+var pais = "Brasil";
+
+if (pais != "Brasil") {
+    console.log("Você é estrangeiro");
+
+} else {
+    console.log("Vocé é brasileiro");
+
+}
+
+var idade = 22;
+
+if (idade < 16) {
+    console.log("Você não vota");
+
+} else if (idade < 18 || idade > 65) {
+    console.log("Voto é opcional");
+
+} else {
+    console.log("Voto obrigatório");
+
+}
