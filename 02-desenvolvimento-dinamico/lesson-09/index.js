c7f4ec58ee161670baa93df7cd00d2536338c993
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-14 21:04:48 -0300

    add switch

---
+++ 02-desenvolvimento-dinamico/lesson-09/index.js
@@ -0,0 +1,22 @@
+//SWITCH
+var diaDaSemana = 4;
+
+switch (diaDaSemana) {
+    case 1:
+        console.log("Domingo");
+
+        break;
+
+    case 2:
+        console.log("Segunda");
+
+        break;
+
+    case 3:
+        console.log("Terça");
+
+    default:
+        console.log("Esse dia da semana não existe");
+
+
+}
