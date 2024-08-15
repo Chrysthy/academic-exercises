31a713f61ff0dcbd09a597406e98385e4dd27f45
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-15 16:47:16 -0300

    add for in

---
+++ 02-desenvolvimento-dinamico/lesson-10/index.js
@@ -0,0 +1,21 @@
+var carro = [
+
+    {
+        modelo: "Audi A3",
+        marca: "Audi",
+        ano: 2020
+    },
+
+    {
+        modelo: "Compass",
+        marca: "Jeep",
+        ano: 2021
+    }
+
+];
+
+for (let caracteristica in carro) {
+
+    console.log(carro[caracteristica]);
+
+}
