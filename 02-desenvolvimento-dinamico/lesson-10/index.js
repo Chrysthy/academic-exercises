c9a9f305352cde01684ea3f0e298621bdb1119d2
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-15 16:54:00 -0300

    add for of

--- 02-desenvolvimento-dinamico/lesson-10/index.js
+++ 02-desenvolvimento-dinamico/lesson-10/index.js
@@ -14,8 +14,17 @@ var carro = [
 
 ];
 
+
+//FOR IN
 for (let caracteristica in carro) {
 
-    console.log(carro[caracteristica]);
+    console.log(carro[caracteristica].marca);
+
+}
+
+
+//FOR OF
+for (let car of carro) {
+    console.log(car.ano);
 
 }
