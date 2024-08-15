8d9ba710ea0270091a4db1641658e28350837f36
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-15 18:19:37 -0300

    add includes, trim, concat e split

--- 02-desenvolvimento-dinamico/lesson-12/index.js
+++ 02-desenvolvimento-dinamico/lesson-12/index.js
@@ -19,3 +19,24 @@ console.log(posicao);
 //SLICE
 var cortado = jogo.slice(8, 16);
 console.log(cortado);
+
+//INCLUDES
+var val = jogo.includes('Portugal');
+console.log(val);
+
+//CONCAT
+var string1 = 'Hello';
+var string2 = ' Chrystine';
+
+var string3 = string1.concat(string2);
+console.log(string3);
+
+//TRIM
+var frase = '   Olá, mundo!   ';
+console.log(frase.trim());
+
+//SPLIT
+var num = '1|2|3|4|5|6|7|8';
+var array = num.split('|');
+
+console.log(array[1]);
