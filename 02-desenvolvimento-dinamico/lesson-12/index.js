40e52e4bc8b1791c102ffdf34d7a448766ec08d6
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-15 18:11:35 -0300

    add lenght, uppercase,index of

--- 02-desenvolvimento-dinamico/lesson-12/index.js
+++ 02-desenvolvimento-dinamico/lesson-12/index.js
@@ -1,1 +1,21 @@
 //STRING METHODS
+//LENGTH
+var nome = 'Chrystine Martins';
+var x = nome.length;
+console.log(x);
+
+resultado = x > 15 ? 'Obrigado' : 'Por favor, preencha seu nome completo'
+console.log(resultado);
+
+//UPPERCASE
+var maiusculo = nome.toUpperCase();
+console.log(maiusculo);
+
+//INDEX OF
+var jogo = 'Real vs Portugal melhor time';
+var posicao = jogo.indexOf('Portugal');
+console.log(posicao);
+
+//SLICE
+var cortado = jogo.slice(8, 16);
+console.log(cortado);
