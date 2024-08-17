971da946b823c456a1fb4f85a5eb034e4f1381ad
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-16 21:37:08 -0300

    add mais strings

--- 02-desenvolvimento-dinamico/lesson-12/index.js
+++ 02-desenvolvimento-dinamico/lesson-12/index.js
@@ -63,3 +63,20 @@ if (frase2.startsWith('Hello')) {
     console.log(novaFraseMaiuscula);
 
 }
+
+
+//MAIS STRINGS
+var url = "http://minhaempresa.com.br/filme?nome=poder&atores=Lucas,Guilherme";
+
+const parametros = url.split('?')[1];
+const valores = parametros.split('&');
+
+console.log(parametros);
+console.log(valores);
+
+for (let i = 0; 1 < valores.length; i++) {
+
+    if (valores[i].startsWith('atores=')) {
+        valores[i] = "atores=" + valores[i].substring(7).toUpperCase();
+    }
+}
