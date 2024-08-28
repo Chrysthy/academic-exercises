d09a9a7fb52f5a896ce6b162ebefb90085ef78cf
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-08-28 15:47:02 -0300

    add viewCart function

--- 02-desenvolvimento-dinamico/lesson-14/index.js
+++ 02-desenvolvimento-dinamico/lesson-14/index.js
@@ -15,4 +15,23 @@ function removeItemFromCart(item) {
         shoppingCart.splice(index, 1);
     }
 
-}
+};
+
+function viewCart() {
+
+    if (shoppingCart.length == 0) {
+
+        console.log("Your shopping cart is empty");
+
+    } else {
+
+        console.log("Itens in your shopping cart: ");
+
+        for (let i = 0; i < shoppingCart.length; i++) {
+
+            console.log(`${i + 1} - ${shoppingCart[i]}`);
+
+        };
+
+    };
+};
