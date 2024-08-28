958b5abcddcdbfa0a5617d65b7a3d4029c5e9c3e
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-08-28 15:34:56 -0300

    add removeItemFromCart function

--- 02-desenvolvimento-dinamico/lesson-14/index.js
+++ 02-desenvolvimento-dinamico/lesson-14/index.js
@@ -1,6 +1,18 @@
 const shoppingCart = [];
 
 function addItemToCard(item) {
+
     shoppingCart.push(item);
+
+};
+
+function removeItemFromCart(item) {
+
+    const index = shoppingCart.indexOf(item);
+
+    if (index !== -1) {
+
+        shoppingCart.splice(index, 1);
+    }
 
 }
