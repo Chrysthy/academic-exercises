2df0b71ec055198c38e6c53c3e0771dbbb1855a2
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-08-29 11:55:46 -0300

    add tratamento de info - if and else if

--- 02-desenvolvimento-dinamico/lesson-16/calcular-imc.js
+++ 02-desenvolvimento-dinamico/lesson-16/calcular-imc.js
@@ -1,6 +1,6 @@
 //Calcular IMC 
 
-function pessoa (nome, sobrenome, a, p) {
+function pessoa(nome, sobrenome, a, p) {
 
     return {
         nome,
@@ -8,8 +8,41 @@ function pessoa (nome, sobrenome, a, p) {
         altura: a,
         peso: p,
 
-        calculoImc () {
-            
+        calculoImc() {
+
+            const indice = this.peso / (this.altura ** 2).toFixed(1);
+
+            if (indice < 15.5) {
+
+                console.log("Abaixo do peso");
+
+
+            } else if (indice >= 18.5 && indice < 24.9) {
+
+                console.log("Peso normal");
+
+
+            } else if (indice >= 25 && indice < 29.9) {
+
+                console.log("Acima do peso");
+
+
+            } else if (indice >= 30 && indice <34.9) {
+
+                console.log("Obesidade 1");
+                
+
+            } else if (indice >= 35 && indice < 39.9) {
+
+                console.log("Obesidade 2");
+                
+
+            } else if (indice >= 40) {
+
+                console.log("Obesidade 3");
+                
+            }
+
         }
 
 
