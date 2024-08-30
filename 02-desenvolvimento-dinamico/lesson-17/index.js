da101975f3a784942e2972172bb7f37e80350b32
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-08-30 14:18:56 -0300

    add constructor

--- 02-desenvolvimento-dinamico/lesson-17/index.js
+++ 02-desenvolvimento-dinamico/lesson-17/index.js
@@ -1,7 +1,14 @@
 //Class
 //criação de classe 
+//para criar um objeto na classe, precisa do constructor
+
 class pessoa {
 
+    constructor(nome, sobrenome) {
 
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+
+    }
 
 };
