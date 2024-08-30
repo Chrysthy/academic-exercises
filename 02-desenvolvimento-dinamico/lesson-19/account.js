8f5b03ef7d5eb572eec7c5686843ebc499ba5699
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-08-30 15:00:40 -0300

    add verificarSaldo function

--- 02-desenvolvimento-dinamico/lesson-19/account.js
+++ 02-desenvolvimento-dinamico/lesson-19/account.js
@@ -1,17 +1,33 @@
 class ContaCliente {
 
-    constructor (numeroConta, saldo, debito, credito) {
+    constructor(numeroConta, saldo, debito, credito) {
 
         this.numeroConta = numeroConta;
         this.saldo = saldo;
         this.debito = debito;
-        this.credito = credito;       
+        this.credito = credito;
 
     };
 
-    calcularSaldoAtual () {
+    calcularSaldoAtual() {
 
-        
+        return this.saldo - this.debito + this.debito
+
+    }
+
+    verificarSaldo() {
+
+        const saldoAtual = this.calcularSaldoAtual()
+
+        if (saldoAtual >= 0) {
+
+            alert("Saldo positivo R$ " + saldoAtual)
+
+        } else {
+
+            alert("Saldo Negativo R$ " + saldoAtual)
+
+        }
 
     }
 
