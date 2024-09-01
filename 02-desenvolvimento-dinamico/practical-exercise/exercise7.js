8d8df9536ba7253359472a74b1101f63012dc07c
Chrystine Martins <chrystine.martins@outlook.com>
2024-08-31 21:05:36 -0300

    add function calcularMediaPonderada

--- 02-desenvolvimento-dinamico/practical-exercise/exercise7.js
+++ 02-desenvolvimento-dinamico/practical-exercise/exercise7.js
@@ -1,1 +1,9 @@
-const readline = require('readline-sync';)
+const readline = require('readline-sync');
+
+function calcularMediaPonderada(notas) {
+    const pesos = [2, 3, 5];
+
+    let somaPesos = 0;
+
+    let somaNotas = 0
+}
