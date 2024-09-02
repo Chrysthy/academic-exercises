23cd0d7abc4e3da56d38e2fa63532b5358262a20
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-02 11:14:21 -0300

    add exercise info

--- 02-desenvolvimento-dinamico/lesson-20/index.js
+++ 02-desenvolvimento-dinamico/lesson-20/index.js
@@ -1,5 +1,19 @@
-window.alert("Nosso primeiro Alert");
+// window.alert("Nosso primeiro Alert");
 
-window.confirm("Deseja excluir esse item?");
+// window.confirm("Deseja excluir esse item?");
 
-window.prompt("Qual é o seu nome?")
+// window.prompt("Qual é o seu nome?")
+
+//Exercício 1
+
+/*
+Crie um prompt informando "Digite seu nome:"
+
+Guarde o nome digitado em uma variável só que em caixa alta.
+
+Crie um outro prompt informando "Digite seu sobrenome:"
+
+Guarde o sobrenome digitado em uma variável só que em caixa alta. 
+
+Apresente o nome de sobrenome digitado em uma janela de alert.
+*/
