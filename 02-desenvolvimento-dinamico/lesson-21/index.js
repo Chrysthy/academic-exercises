18e9056083a808de1860707549d1bb0a9b5cb4a5
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-02 14:26:32 -0300

    add finction confirm

---
+++ 02-desenvolvimento-dinamico/lesson-21/index.js
@@ -0,0 +1,14 @@
+function confirma () {
+
+    let confirmacao = confirm ("Você deseja excluir esse item?");
+
+    if (confirmacao) {
+
+        alert("Você irá excluir o item")
+
+    } else {
+
+        alert("Operação foi cancelada")
+    }
+
+}
