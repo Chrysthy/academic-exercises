b3fbcb53de165e8e9ac9bd3e0312fd0884258cfc
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-03 08:44:26 -0300

    add innerText terceiro li

--- 02-desenvolvimento-dinamico/lesson-22/index.js
+++ 02-desenvolvimento-dinamico/lesson-22/index.js
@@ -3,3 +3,6 @@ elemento0.style.color = 'blue';
 
 elemento1 = document.getElementsByTagName('li')[1];
 elemento1.style.background = 'red';
+
+elemento2 = document.getElementsByTagName('li')[2];
+elemento2.innerText = "SpringBoot"
