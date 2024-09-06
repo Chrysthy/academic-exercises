711906cd3a6e60c547b8203acc619bae3f24e872
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-06 09:14:42 -0300

    add for to listEstilo

--- 02-desenvolvimento-dinamico/lesson-23/index.js
+++ 02-desenvolvimento-dinamico/lesson-23/index.js
@@ -1,1 +1,9 @@
 const listEstilo = document.querySelectorAll(".estilo");
+
+for (let i = 0; i < listEstilo.length; i++) {
+
+    listEstilo[i].style.backgroundColor = "blue";
+    listEstilo[i].style.color = "white";
+    listEstilo[i].innerText = listEstilo[i].innerText.toUpperCase();
+
+}
