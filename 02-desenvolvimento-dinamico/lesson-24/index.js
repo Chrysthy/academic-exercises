ead01a70ccb2c2594e827f2b9970ecaa5edba055
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-06 10:05:50 -0300

    add const to the js file

---
+++ 02-desenvolvimento-dinamico/lesson-24/index.js
@@ -0,0 +1,7 @@
+const elemento = document.createElement("div");
+
+elemento.innerText = "Incluíndo texto na div com o JS";
+elemento.style.backgroundColor = "lightgreen";
+
+// append adicionar ao body
+document.body.append(elemento)
