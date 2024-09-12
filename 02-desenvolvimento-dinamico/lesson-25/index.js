87b48c59e1dcbc4a8991f9633b71868de80c00e9
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-12 20:46:36 -0300

    add function mouseOut

--- 02-desenvolvimento-dinamico/lesson-25/index.js
+++ 02-desenvolvimento-dinamico/lesson-25/index.js
@@ -1,8 +1,8 @@
 //Trabalhando com Eventos
 function exemploEvento() {
-    alert ('mostrando o alert')
+    alert('mostrando o alert')
 }
 
 function passarMousePorCima() {
-    
+    document.getElementById('exemplo').innerHTML = 'Evento com o Mouse over'
 }
