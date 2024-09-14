94b89e23bb85d45683b900ef1c5cbb6d166e95f6
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-14 16:19:04 -0300

    add exemple 2 try and catch

--- 02-desenvolvimento-dinamico/lesson-26/index.js
+++ 02-desenvolvimento-dinamico/lesson-26/index.js
@@ -1,5 +1,5 @@
 //try, catch e finally
-function divisao(a, b) {
+/*function divisao(a, b) {
 
     if (b === 0) {
 
@@ -20,3 +20,29 @@ try {
     console.log("Ocorreu uma execução: " + error.message);
     
 }
+*/
+
+//Exemplo 2
+const numeros = [1, 2, "3", 4, 5];
+
+try {
+
+    const doubleNumbers = numeros.map((num) => {
+
+        if (typeof num !== 'number') {
+
+            throw new Error('O array só pode ter números')
+        }
+
+        return num * 2;
+    })
+
+    console.log(doubleNumbers);
+
+
+} catch (error) {
+
+    console.log('Ocorreu uma exceção: ' + error.message);
+
+}
+
