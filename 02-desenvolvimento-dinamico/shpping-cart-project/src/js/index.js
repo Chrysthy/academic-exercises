c8a1462390f21e0ea3b30e8f27933ce36489d979
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-14 13:02:21 -0300

    add adicionarItem function

--- 02-desenvolvimento-dinamico/shpping-cart-project/src/js/index.js
+++ 02-desenvolvimento-dinamico/shpping-cart-project/src/js/index.js
@@ -1,3 +1,17 @@
 var valorTotal = [0, 0];
 var valorProduto = [50.00, 30.00];
-var quantidade = [0, 0]; 
+var qtd = [0, 0];
+
+function adicionarItem(item) {
+
+    var quantidade = document.getElementById('quantidade' + item);
+    var total = document.getElementById('total' + item);
+
+    qtd[item] += 1;
+
+    valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+
+    quantidade.innerHTML = qtd[item];
+
+    total.innerHTML = valorProduto[item].toFixed(2);
+}
