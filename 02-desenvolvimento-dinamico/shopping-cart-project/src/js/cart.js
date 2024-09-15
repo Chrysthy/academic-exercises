e77316f2344bffc986a0204bc52aa01efe12649b
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-15 17:36:10 -0300

    add function start and fetch

---
+++ 02-desenvolvimento-dinamico/shopping-cart-project/src/js/cart.js
@@ -0,0 +1,19 @@
+let carrinho = [];
+
+function start() {
+
+    fetch('https://fakestoreapi.com/products?limit=10')
+
+        .then(response => response.json())
+
+        .then(data => {
+            preencherCarrinho(data)
+        })
+
+        .catch(error => {
+
+            console.log('Ocorreu um erro', error);
+
+        })
+
+}
