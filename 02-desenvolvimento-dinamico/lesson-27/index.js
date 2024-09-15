34e65e407be7f60d8ae72a4de0cf4a2e7b6bc72b
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-15 14:22:13 -0300

    add segundo exemplo promise

--- 02-desenvolvimento-dinamico/lesson-27/index.js
+++ 02-desenvolvimento-dinamico/lesson-27/index.js
@@ -1,5 +1,6 @@
 //Promise
-const minhaPromise = new Promise((resolve, reject) => {
+//exemplo1
+/*const minhaPromise = new Promise((resolve, reject) => {
 
     const sucesso = false
 
@@ -28,4 +29,36 @@ minhaPromise
 
         console.log('Deu erro: ' + erro);
         
+    })
+*/
+
+//Exemplo 2
+function somarArray(array) {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Array.isArray(array)) {
+
+            reject('O argumento passado não é um array')
+        }
+
+        const soma = array.reduce((total, num) => total + num, 0)
+
+        resolve(soma)
+
+    })
+}
+
+const myArray = [1, 2, 3, 4, 5]
+somarArray(myArray)
+    .then((resultado) => {
+
+        console.log('A soma dos elementos é: ', resultado);
+
+    })
+
+    .catch((erro) => {
+
+        console.log('Ocorreu um erro: ' + erro);
+
     })
