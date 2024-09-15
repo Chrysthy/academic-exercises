abbb6b9d1eb9808999d43ece1b85c9dd703dee04
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-15 16:28:53 -0300

    add json example 2

--- 02-desenvolvimento-dinamico/lesson-28/index.js
+++ 02-desenvolvimento-dinamico/lesson-28/index.js
@@ -1,12 +1,43 @@
 //JSON
-const pessoa = {
+/*const pessoa = {
 
     nome: 'Chrystine',
     idade: 32,
     profissao: 'Desenvolvedora'
 }
 
+//passando para Json
 const pessoaJson = JSON.stringify(pessoa);
 
 console.log(pessoa);
 console.log(pessoaJson);
+
+//voltando para objeto
+const pessoaObj = JSON.parse(pessoaJson)
+
+console.log(pessoaObj);
+*/
+
+//Exemplo 2
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+
+        preencherLista(data)
+    })
+
+function preencherLista(data) {
+
+    const listaContainer = document.getElementById('lista-container');
+    const ul = document.createElement('ul');
+
+    data.array.forEach(item => {
+
+        const li = document.createElement('li');
+        li.textContent = item.name;
+        ul.appendChild(li);
+    });
+
+    listaContainer.appendChild(ul)
+
+}
