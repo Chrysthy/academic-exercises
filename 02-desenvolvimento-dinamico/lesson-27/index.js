5e5b4ca34526ae0f84b0cbfd058c51d7f37c3dd7
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-15 15:00:09 -0300

    add quarto exemplo promise

--- 02-desenvolvimento-dinamico/lesson-27/index.js
+++ 02-desenvolvimento-dinamico/lesson-27/index.js
@@ -66,7 +66,7 @@ somarArray(myArray)
 */
 
 //Exemplo 3
-class MinhaClasse {
+/*class MinhaClasse {
 
     constructor(valor) {
 
@@ -104,5 +104,61 @@ minhaClasse.operacaoAssincrona()
     .catch((erro) => {
 
         console.log("Ocorreu um problema:", erro);
+
+    })
+*/
+
+//Exemplo 4
+function obterValor(valor) {
+    return new Promise((resolve, reject) => {
+
+        if (valor > 10) {
+
+            valor += 1;
+
+            resolve(valor)
+
+        } else {
+
+            reject('Não foi possível obter o valor')
+        };
+
+    });
+};
+
+function dobrarValor(valor) {
+    return valor * 2
+};
+
+function adicionar10(valor) {
+    return valor + 10
+};
+
+obterValor(28)
+    .then((valor) => {
+
+        console.log("Valor obtido");
+
+        return dobrarValor(valor)
+    })
+
+    .then((novoValor) => {
+
+        console.log("Fazendo o segundo tratamento");
+
+        return adicionar10(novoValor)
+
+    })
+
+    .then((resultado) => {
+
+        total = resultado + 5
+        console.log("O resultado final é", total);
+
+    })
+
+    .catch((erro) => {
+
+        console.log("Ocorreuum erro:", erro);
 
     })
