348cb6b92dbd0d5778c906ae9807f64ab8d3a354
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-15 16:01:48 -0300

    add json example

---
+++ 02-desenvolvimento-dinamico/lesson-28/index.js
@@ -0,0 +1,12 @@
+//JSON
+const pessoa = {
+
+    nome: 'Chrystine',
+    idade: 32,
+    profissao: 'Desenvolvedora'
+}
+
+const pessoaJson = JSON.stringify(pessoa);
+
+console.log(pessoa);
+console.log(pessoaJson);
