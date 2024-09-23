14ab3418cefa3b9511f76308bf3cda8efb28b22d
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-23 14:24:37 -0300

    add removeValue

--- cursos-complementares/introdução-ao-desenvolvimento-web/src/index.js
+++ cursos-complementares/introdução-ao-desenvolvimento-web/src/index.js
@@ -2,5 +2,12 @@ function getValue() {
 
     let value = document.getElementById("name").value;
 
-    document.getElementById("result").innerHTML = `Bem vindo(a), ${value}`
+    document.getElementById("result").innerHTML = `Bem vindo(a), ${value}!`
+}
+
+function removeValue() {
+    
+    document.getElementById("name").value = "";
+    document.getElementById("result").innerHTML = "Até a próxima!"
+
 }
