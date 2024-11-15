9beccc8ef70120736db773a092fa5331d5930a8e
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-15 09:17:19 -0300

    add exercise3

---
+++ 03-logica-de-programacao/lesson-13-java/exercise3.java
@@ -0,0 +1,21 @@
+class exercise3 {
+
+    public static void main(String entrada[])
+
+    {
+        int inteiro = 32;
+        char caractere = 'F';
+        double real = 1.60;
+        String frase = "Chrystine Vasconcelos";
+        boolean VF = true;
+
+        if (VF == true) {
+
+            System.out.println("Eu sou a " + frase + ", tenho " + inteiro + " anos, sou do sexo " + caractere + " e tenho " + real + " m de altura.");
+
+        }
+
+        System.exit(0);// para o programa
+    }
+
+}
