5566a8a106c444f7b89c609ace0b69a99ee69213
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-15 08:08:48 -0300

    add primeiro programa java

--- 03-logica-de-programacao/lesson-13-java/exercise.java
+++ 03-logica-de-programacao/lesson-13-java/exercise.java
@@ -1,1 +1,24 @@
 //Meu primeiro programa em Java
+//nome da classe
+
+class MeuPrimeiroProgramaJava {
+
+    //módulo principal com a entrada pela linha de comando
+    public static void main (String entrada[])
+
+    {
+        //declaraçãpo de variáveis
+        int inteiro - 32;
+        char caractere = 'F';
+        double real = 1.60;
+        String frase = "Chrystine Vasconcelos";
+        boolean VF = true;
+
+        if (VF == true) {
+
+            System.out.printIn("Eu sou a " + frase + ", tenho " + inteiro + "anos e tenho " + real + "m de altura.");
+
+        }
+
+    }
+}
