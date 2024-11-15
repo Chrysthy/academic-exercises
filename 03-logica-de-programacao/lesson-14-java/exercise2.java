ee4e454cb267aaa126d88a741f6f6d35db6117f8
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-15 15:44:24 -0300

    add resto da divisão

--- 03-logica-de-programacao/lesson-14-java/exercise2.java
+++ 03-logica-de-programacao/lesson-14-java/exercise2.java
@@ -2,12 +2,25 @@ class Programa02 {
     public static void main(String entrada[]) {
 
         int n1, n2;
-        int mod, dic;
+        int mod, div;
         double raiz, pot;
         String msg = "";
 
+        n1 = Integer.parseInt(entrada[0]);
+        n2 = Integer.parseInt(entrada[1]);
 
+        mod = n1 % n2;
+        div = (int) n1 / (int) n2;
+        raiz = Math.sqrt(n1);
+        pot = Math.pow(n1, n2);
 
+        msg = "n1  = " + n1 + " n2 = " + n2 + "\n";
+        msg = msg + "resto da divisão de n1  por n2 = " + mod + "\n";
+       
+
+        System.out.println(msg);
+
+        System.exit(0);
 
     }
 
