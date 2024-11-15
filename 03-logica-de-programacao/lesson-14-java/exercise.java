8e5976635f1d661b98d0a9b80f2cc59afc97c60e
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-15 15:16:10 -0300

    add public static

--- 03-logica-de-programacao/lesson-14-java/exercise.java
+++ 03-logica-de-programacao/lesson-14-java/exercise.java
@@ -1,3 +1,20 @@
 class Programa01 {
- 
+    public static void main(String entrada[])
+
+    {
+        int NumInt;
+        double NumReal, soma;
+        char Caractere;
+
+        NumInt = Integer.parseInt(entrada[0]);
+        NumReal = Double.parseDouble(entrada[1]);
+        Caractere = entrada[2].charAt(0);
+
+        soma = (double) NumInt + NumReal;
+
+        System.out.println((double) NumInt + " + " + NumReal + " = " + soma + " sinal " + Caractere);
+
+        System.exit(0);
+
+    }
 }
