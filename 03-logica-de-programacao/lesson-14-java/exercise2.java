1ca310dd1ab4b366e06fda5d7c6d7dc7b05ace14
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-15 15:39:25 -0300

    add class Programa02

---
+++ 03-logica-de-programacao/lesson-14-java/exercise2.java
@@ -0,0 +1,14 @@
+class Programa02 {
+    public static void main(String entrada[]) {
+
+        int n1, n2;
+        int mod, dic;
+        double raiz, pot;
+        String msg = "";
+
+
+
+
+    }
+
+}
