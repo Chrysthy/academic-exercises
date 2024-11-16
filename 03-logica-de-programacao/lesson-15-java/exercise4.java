b4642df51b341893e0f3a6a57e96516625c76de2
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-16 18:21:32 -0300

    add variáveis

--- 03-logica-de-programacao/lesson-15-java/exercise4.java
+++ 03-logica-de-programacao/lesson-15-java/exercise4.java
@@ -1,6 +1,18 @@
 import javax.swing.*;
 
 class Prog06 {
-   
+    public static void main(String entrada[]) {
+
+        int n1, n2, p = 0;
+        char op = 0;
+        String msg = "", msgEntr = "Digite 1 para produto\nDigite 2 para produtória\n";
+
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro"));
+
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número inteiro"));
+
+        op = JOptionPane.showInputDialog(msgEntr).charAt(0);
+
+ 
     }
 }
