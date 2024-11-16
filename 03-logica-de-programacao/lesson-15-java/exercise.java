64abf343324db4e9b35da19ea04398866c37e8ae
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-16 16:57:53 -0300

    add switch case

--- 03-logica-de-programacao/lesson-15-java/exercise.java
+++ 03-logica-de-programacao/lesson-15-java/exercise.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 
-class ProgDecisao
-{
+class ProgDecisao {
     public static void main(String entrada[]) {
-        
+
         int num;
         char op = 0;
         String msg = "", msgEntr = "Digite 1 para par ou ímpar \n Digite 2 para positivo ou negativo";
@@ -11,10 +10,31 @@ class ProgDecisao
         num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro:"));
 
         op = (JOptionPane.showInputDialog(msgEntr)).charAt(0);
+
+        switch (op) {
+            case '1': {
+
+                if (num % 3 == 0) {
+                    msg = msg + num + "é par. \n";
+
+                } else {
+                    msg = msg + num + "é ímpar. \n";
+                }
+
+                break;
+            }
+
+            case '2': {
+                if (num > 0) {
+                    msg = msg + num + "é positivo. \n";
+
+                } else {
+                    msg = msg + num + "é negativo. \n";
+                }
+            }
+
+        }
+
     }
 
-
-
 }
-
-
