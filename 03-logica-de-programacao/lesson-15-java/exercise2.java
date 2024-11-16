7d33aad24dc3f53be6b57864daecdc732e2ce620
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-16 17:27:33 -0300

    add variáveis

--- 03-logica-de-programacao/lesson-15-java/exercise2.java
+++ 03-logica-de-programacao/lesson-15-java/exercise2.java
@@ -1,3 +1,25 @@
 import javax.swing.*;
 
 class ProgRepeticao {
+    public static void main(String entrada[]) {
+
+        int Tabuada;
+        char op = 0;
+        String msg = "",
+                msgEntr = "Digite 1 para repeticao for.\nDigite 2 para repeticao while.\nDigite 3 para repeticao do while.";
+
+        Tabuada = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro: "));
+
+        op = (JOptionPane.showInputDialog(msgEntr)).charAt(0);
+
+        switch (op) {
+           
+        }
+
+
+
+
+
+    }
+
+}
