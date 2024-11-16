d76b6be4c1f7f66f94662261cb7c936945b84cc2
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-16 18:02:59 -0300

    add variáeis

--- 03-logica-de-programacao/lesson-15-java/exercise3.java
+++ 03-logica-de-programacao/lesson-15-java/exercise3.java
@@ -3,6 +3,16 @@ import javax.swing.*;
 class Exercise3 {
     public static void main(String entrada[]) {
 
-    
+        int n1, n2, soma = 0;
+        char op = 0;
+        String msg = "", msgEntr = "Digite 1 para adicao/subtracao\nDigite 2 para somatoria\n";
+
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro"));
+
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número inteiro"));
+
+        op = JOptionPane.showInputDialog(msgEntr).charAt(0);
+
+ 
     }
 }
