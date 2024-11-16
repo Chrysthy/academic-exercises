dd48de0b2f184f0b1fe57a9b31d0e262bfa6b208
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-16 16:52:22 -0300

    add variáveis

--- 03-logica-de-programacao/lesson-15-java/exercise.java
+++ 03-logica-de-programacao/lesson-15-java/exercise.java
@@ -1,6 +1,20 @@
 import javax.swing.*;
 
 class ProgDecisao
+{
+    public static void main(String entrada[]) {
+        
+        int num;
+        char op = 0;
+        String msg = "", msgEntr = "Digite 1 para par ou ímpar \n Digite 2 para positivo ou negativo";
 
+        num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro:"));
+
+        op = (JOptionPane.showInputDialog(msgEntr)).charAt(0);
+    }
+
+
+
+}
 
 
