4f23e8bc5d41e9ca480801ad52e4c539ad275761
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-22 21:04:19 -0300

    add variáveis

--- 03-logica-de-programacao/lesson-16-java/exercise.java
+++ 03-logica-de-programacao/lesson-16-java/exercise.java
@@ -1,1 +1,14 @@
 import javax.swing.*;
+
+class ProgMatriz
+{
+    public static void main (String entrada[])
+
+    {
+
+        int vetor[] = { 2, 4, 6, 8, 10 };
+        int matriz[][] = new int[2][3];
+        String msg = "vetor = ";
+
+    }
+}
