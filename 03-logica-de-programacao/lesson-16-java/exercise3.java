a5cda3760c6c0cced3d764afe38408e4aa2ed79e
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-22 22:00:52 -0300

    add public static void soma

--- 03-logica-de-programacao/lesson-16-java/exercise3.java
+++ 03-logica-de-programacao/lesson-16-java/exercise3.java
@@ -2,6 +2,17 @@ import javax.swing.JOptionPane;
 
 class Prog07 {
 
+    public static void soma() {
+        int s = 0, vet[] = { 1, 2, 3, 4, 5, };
+
+        for (int i = 0; i < vet.length; i++) {
+            s = s + vet[i];
+        }
+
+        JOptionPane.showMessageDialog(null, "A somatória é = " + s);
+
+    }
+
     
 
 }
