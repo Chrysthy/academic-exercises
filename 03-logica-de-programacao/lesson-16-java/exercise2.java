e646d13721e2a0b028bf2abb4dd4b021856fb8de
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-22 21:46:01 -0300

    add class ProgMod and public static void soma

--- 03-logica-de-programacao/lesson-16-java/exercise2.java
+++ 03-logica-de-programacao/lesson-16-java/exercise2.java
@@ -1,1 +1,16 @@
 import javax.swing.*;
+
+class ProgMod {
+
+    public static void soma() {
+        int n1, n2;
+
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número: "));
+
+        JOptionPane.showMessageDialog(null, "A soma é: " + (n1 + n2));
+
+    }
+
+
+}
