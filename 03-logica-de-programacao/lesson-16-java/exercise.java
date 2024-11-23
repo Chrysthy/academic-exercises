5ab68067d8dcdc58a1eb3230198b64477f699524
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-22 21:06:07 -0300

    add vetor

--- 03-logica-de-programacao/lesson-16-java/exercise.java
+++ 03-logica-de-programacao/lesson-16-java/exercise.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
 
-class ProgMatriz
-{
-    public static void main (String entrada[])
+class ProgMatriz {
+    public static void main(String entrada[])
 
     {
 
         int vetor[] = { 2, 4, 6, 8, 10 };
         int matriz[][] = new int[2][3];
         String msg = "vetor = ";
+
+        for (int i = 0; i < vetor.length; i++) {
+
+            msg = msg + vetor[i] + " ";
+        }
+
+        JOptionPane.showMessageDialog(null, msg);
 
     }
 }
