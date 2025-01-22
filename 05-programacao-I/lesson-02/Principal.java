62d9ae8690f2e45818da6b1d8f74f7c002724618
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-22 20:48:48 -0300

    add example-01

---
+++ 05-programacao-I/lesson-02/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println(9 + 2);
+		System.out.println(9 - 2);
+		System.out.println(9 * 2);
+		System.out.println(9 / 2);
+		System.out.println(9.0 / 2);
+
+	}
+
+}
