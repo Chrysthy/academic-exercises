77b2d8c5af07e621fbb8a25ecd90259bc74d6409
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-22 20:54:41 -0300

    add compilação

---
+++ 05-programacao-I/lesson-02/Exercise02.java
@@ -0,0 +1,22 @@
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		
+		byte x = 3;
+		short y = 4;
+		int z = 5;
+		long k = 6;
+		
+		double w = 3.0;
+		boolean t = true;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(k);
+		
+		System.out.println(w);
+		System.out.println(t);
+	}
+
+}
