195bed54bfb89c93af09673ca6225a0b7a0f2d1e
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-25 17:19:20 -0300

    add operadores example

---
+++ 05-programacao-I/lesson-02/Exercise06.java
@@ -0,0 +1,16 @@
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		
+		int numero;
+		
+		numero = 6;
+		
+		numero += 2; //numero = numero + 2;
+		
+		System.out.println(numero);
+
+	}
+
+}
