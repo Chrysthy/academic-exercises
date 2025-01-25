5413b7a28e27062b324455dab516456ffe6ea0d1
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-25 17:53:10 -0300

    add new content

--- 05-programacao-I/lesson-02/Exemplo06.java
+++ 05-programacao-I/lesson-02/Exemplo06.java
@@ -2,16 +2,23 @@
 public class Exemplo06 {
 
 	public static void main(String[] args) {
-		
-		int valor = 8;
 
-		if (valor == 9) {
+		int valor = 9;
+
+		if (valor == 8) {
 
 			System.out.println("Valor é igual a 8");
 
 		} else {
 
-			System.out.println("Valor é diferente de 8");
+			if (valor > 8) {
+
+				System.out.println("Valor é maior de 8");
+
+			} else {
+
+				System.out.println("Valor é menor de 8");
+			}
 		}
 
 	}
