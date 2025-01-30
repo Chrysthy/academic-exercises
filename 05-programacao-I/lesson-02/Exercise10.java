a282eace80294d692d35c9e20f3c0ae2d63b759f
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-30 20:51:07 -0300

    add example 10

---
+++ 05-programacao-I/lesson-02/Exercise10.java
@@ -0,0 +1,20 @@
+
+public class Exercise10 {
+
+	public static void main(String[] args) {
+		
+	int numero = 1;
+	
+	while(numero <= 5) {
+		
+		System.out.println(numero);
+		numero++;
+		
+		}	
+			
+	
+	}
+
+}
+
+
