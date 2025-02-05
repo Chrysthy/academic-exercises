f18913843c230b40fe39c476b0182e7b8260927f
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-04 21:41:55 -0300

    add abstração em java

---
+++ 07-Design-de-software/lesson-03/Exercise.java
@@ -0,0 +1,14 @@
+public class Funcionario {
+    public String nome;
+    public String cargo;
+    private float salario;
+
+
+    public float calcularBonus(){
+
+    }
+
+    private float aumentarSalario(){
+
+    }
+}
