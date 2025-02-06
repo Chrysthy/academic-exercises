186f2ac4fae9af618101f55ecb3a723b00797fa7
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-05 21:31:10 -0300

    update code

--- 06-estrutura-de-dados/lesson-05/Exercise.java
+++ 06-estrutura-de-dados/lesson-05/Exercise.java
@@ -1,66 +1,29 @@
 //recursão sem cauda
 
-import javax.swing;
-
- 
+import javax.swing.JOptionPane;
 
 class FatorialP {
 
-  static void fatP (int n, int x, int f)
-
-    {
-
-      if (x == 0 || x == 1)  {
-
-          System.out.println("O fatorial de " + n + " é " + f); // regra 1
-
-        }
-
-      else
-
-        {
-
-          // chamada da função recursiva
-
-          fatP(n, x-1, fx); // regra 2 e 3
-
-        }
-
-    }
-
-public static void main (String arg [ ])
-
-        {
-
-            int nro;
-
- 
-
-            nro = Integer.parseInt(JOptionPane.showInputDialog(“Digite um valor que você deseja saber o fatorial”));
-
-            if (nro < 0)
-
-               {
-
-                  System.out.println (“Valor inválido para cálculo de fatorial, o valor precisa ser maior ou igual a zero”);
-
-                  System.exit(0);
-
-               }
-
-            else
-
-               {
-
-                  fatP(nro, nro, 1); // chamada da função recursiva
-
-               }
-
-            System.exit(0);
-
-         }
-
+   static void fatP(int n, int x, int f) {
+      if (x == 0 || x == 1) {
+         System.out.println("O fatorial de " + n + " é " + f);
+      } else {
+         fatP(n, x - 1, f * x);
+      }
    }
 
- 
- 
+   public static void main(String[] args) {
+      int nro;
+
+      nro = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor que você deseja saber o fatorial"));
+
+      if (nro < 0) {
+         System.out.println("Valor inválido para cálculo de fatorial, o valor precisa ser maior ou igual a zero");
+         System.exit(0);
+      } else {
+         fatP(nro, nro, 1);
+      }
+
+      System.exit(0);
+   }
+}
