eb82166a70226df22644c1149091a33ace90db46
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-06 20:43:59 -0300

    add exercise 1

---
+++ 05-programacao-I/lesson-04/Exercise01.java
@@ -0,0 +1,12 @@
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+
+		//cadeia de caracteres
+		String nome = "Chrystine Martins";
+		
+		System.out.println("Aluna " + nome);
+
+	}
+}
