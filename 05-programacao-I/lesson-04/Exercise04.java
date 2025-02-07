349a08d23f8be846b3f0697e94e7c37356648432
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-06 21:30:28 -0300

    add exercise 4

---
+++ 05-programacao-I/lesson-04/Exercise04.java
@@ -0,0 +1,13 @@
+public class Exercise04 {
+
+	public static void main(String[] args) {
+
+		int[] nota = {9, 10, 8};
+
+			for (int i = 0; i < 3; i++) {
+
+			System.out.println(nota[i]);
+
+		}
+	}
+}
