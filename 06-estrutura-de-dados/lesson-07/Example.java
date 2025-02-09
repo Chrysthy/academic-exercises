afdbd3e7bdf0b8ca25dad7063c64406ed9171190
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-09 15:38:01 -0300

    add class pilha -biblioteca

--- 06-estrutura-de-dados/lesson-07/Example.java
+++ 06-estrutura-de-dados/lesson-07/Example.java
@@ -1,72 +1,86 @@
+class Pilha {
 
-public boolean PilhaVazia() {
+    int tamanho;
+    int topo;
+    int vetor[];
 
-    if (topo == -1) {
-        return true;
+    Pilha(int tam) {
 
-    } else {
-        return false;
-    }
-}
-
-public boolean PilhaCheia() {
-
-    if (topo >= tamanho - 1) {
-        return true;
-
-    } else {
-        return false;
-    }
-}
-
-public void Empilhar(int elemento) {
-
-    if (!PilhaCheia()) {
-
-        topo = topo + 1;
-        vetor[topo] = elemento;
-
-    } else {
-        System.out.println("Pilha cheia");
-    }
-}
-
-public int Desempilhar() {
-
-    int desempilhado = 0;
-
-    if (PilhaVazia()) {
-
-        System.out.println("Pilha Vazia");
-
-    } else {
-
-        desempilhado = vetor[topo];
-        topo = topo - 1;
-
+        topo = -1;
+        tamanho = tam;
+        vetor = new int[tam];
     }
 
-    return desempilhado;
-}
+    public boolean PilhaVazia() {
 
-public void ElementoTopo() {
+        if (topo == -1) {
+            return true;
 
-    if (topo >= 0) {
-
-        System.out.println("O elemento do topo é " + vetor[topo]);
-
-    } else {
-
-        System.out.println("Pilha vazia");
+        } else {
+            return false;
+        }
     }
-}
 
-public void MostrarPilha() {
+    public boolean PilhaCheia() {
 
-    int i;
+        if (topo >= tamanho - 1) {
+            return true;
 
-    for (i = topo; i >= 0; i--) {
-
-        System.out.println("Elemento " + vetor[i] + "posição " + i + " da pilha");
+        } else {
+            return false;
+        }
     }
-}
+
+    public void Empilhar(int elemento) {
+
+        if (!PilhaCheia()) {
+
+            topo = topo + 1;
+            vetor[topo] = elemento;
+
+        } else {
+            System.out.println("Pilha cheia");
+        }
+    }
+
+    public int Desempilhar() {
+
+        int desempilhado = 0;
+
+        if (PilhaVazia()) {
+
+            System.out.println("Pilha Vazia");
+
+        } else {
+
+            desempilhado = vetor[topo];
+            topo = topo - 1;
+
+        }
+
+        return desempilhado;
+    }
+
+    public void ElementoTopo() {
+
+        if (topo >= 0) {
+
+            System.out.println("O elemento do topo é " + vetor[topo]);
+
+        } else {
+
+            System.out.println("Pilha vazia");
+        }
+    }
+
+    public void MostrarPilha() {
+
+        int i;
+
+        for (i = topo; i >= 0; i--) {
+
+            System.out.println("Elemento " + vetor[i] + "posição " + i + " da pilha");
+        }
+    }
+
+}   
