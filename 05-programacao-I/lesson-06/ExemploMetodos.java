4a15834d84e7db908fc6d2990f62b43f200ac810
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-15 16:18:53 -0300

    add exemplo métodos

---
+++ 05-programacao-I/lesson-06/ExemploMetodos.java
@@ -0,0 +1,24 @@
+
+public class ExemploMetodos {
+
+	public static void main(String[] args) {
+
+		String nome = "Chrystine Martins";
+		
+		System.out.println(nome);
+		
+		System.out.println(nome.length());
+		System.out.println(nome.toLowerCase());
+		System.out.println(nome.toUpperCase());
+		System.out.println(nome.charAt(2));
+		System.out.println(nome.isEmpty());
+		System.out.println(nome.indexOf("tine"));
+		System.out.println(nome.contains("tine"));
+		System.out.println(nome.concat("de Oliveira"));
+		System.out.println(nome.equals("chrystine martins"));
+		System.out.println(nome.equalsIgnoreCase("chrystine martins"));
+
+	}
+
+}
+
