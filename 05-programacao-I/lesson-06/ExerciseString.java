836a6eba8d38209aafb9bebcc879a543c9dbd803
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-15 09:31:25 -0300

    add example 1

---
+++ 05-programacao-I/lesson-06/ExerciseString.java
@@ -0,0 +1,17 @@
+public class ExerciseString {
+
+	public static void main(String[] args) {
+		
+		String nome = "Chrystine";
+		String sobrenome = new String("Martins");
+		
+		
+		System.out.println(nome);
+		System.out.println(sobrenome);
+		
+		System.out.println(nome.length());
+		System.out.println(sobrenome.length());
+
+	}
+
+}
