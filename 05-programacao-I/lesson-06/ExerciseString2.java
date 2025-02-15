d1773096198983efb10cda3897d39fc3a3569a40
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-15 16:08:57 -0300

    add comparações

---
+++ 05-programacao-I/lesson-06/ExerciseString2.java
@@ -0,0 +1,40 @@
+public class ExerciseString2 {
+
+	public static void main(String[] args) {
+		
+		//criado com atribuição
+		String nome = "Chrystine";
+		String nomeGuerra = "Chrystine";
+		String sobrenome = new String("Chrystine");
+		//criado instanciando explicitamente
+		
+		System.out.println(nome);
+		System.out.println(sobrenome);
+		
+		
+		
+		if(nome.equals(sobrenome)) {
+			
+			System.out.println("Nome Iguais");
+			
+		} else {
+			
+			System.out.println("Nomes Diferentes");
+		}
+		
+		
+		if(nome.equals(sobrenome)) {
+			
+			System.out.println("Nome Iguais");
+			
+		} else {
+			
+			System.out.println("Nomes Diferentes");
+		}
+			
+
+	}
+
+}
+
+
