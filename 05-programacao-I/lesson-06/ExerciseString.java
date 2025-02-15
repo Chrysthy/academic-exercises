b06ef0177f995f46737996e03ea40b1e7e2a94d6
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-15 16:11:57 -0300

    add String para comparação

--- 05-programacao-I/lesson-06/ExerciseString.java
+++ 05-programacao-I/lesson-06/ExerciseString.java
@@ -1,17 +1,35 @@
 public class ExerciseString {
 
-	public static void main(String[] args) {
-		
-		String nome = "Chrystine";
-		String sobrenome = new String("Martins");
-		
-		
-		System.out.println(nome);
-		System.out.println(sobrenome);
-		
-		System.out.println(nome.length());
-		System.out.println(sobrenome.length());
+    public static void main(String[] args) {
 
-	}
+        String nome = "Chrystine";
+        String nomeGuerra = "Chrystine";
+        String sobrenome = new String("Martins");
+
+        System.out.println(nome);
+        System.out.println(sobrenome);
+
+        if (nome == nomeGuerra) {
+
+            System.out.println("Nome Iguais");
+
+        } else {
+
+            System.out.println("Nomes Diferentes");
+        }
+
+        if (nome == sobrenome) {
+
+            System.out.println("Nome Iguais");
+
+        } else {
+
+            System.out.println("Nomes Diferentes");
+        }
+
+        System.out.println(nome.length());
+        System.out.println(sobrenome.length());
+
+    }
 
 }
