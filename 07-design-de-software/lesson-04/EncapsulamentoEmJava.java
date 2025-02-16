58830928df69c4b02add629e5b5ccb1180be0c84
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-16 09:03:56 -0300

    add encampsulamento em java

---
+++ 07-design-de-software/lesson-04/EncapsulamentoEmJava.java
@@ -0,0 +1,45 @@
+public class EncapsulamentoEmJava {
+
+    public class Aluno {
+
+        private String nome;
+
+        private int idade;
+
+        public String getNome() {
+            return null;
+        }
+
+        public int getIdade() {
+            return 0;
+        }
+
+        public void setNome(String nome) {
+
+        }
+
+        public void setIdade(int idade) {
+
+        }
+
+    }
+
+    public class Curso {
+
+        private String nome;
+    
+        private int creditos;
+    
+        private int disciplinas;
+    
+        public String getCurso() {
+            return null;
+        }
+    
+        public String setCurso() {
+            return null;
+        }
+    
+    }
+    
+}
