0ef47d11ff86d22940f7a6322844f5e5c078fe36
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-19 20:57:03 -0300

    add vector

---
+++ 05-programacao-I/lesson-07/vector/Collection.java
@@ -0,0 +1,24 @@
+
+import java.util.Vector;
+
+public class Collection {
+
+	public static void main(String[] args) {
+
+		Vector<String> listaNomes = new Vector<>();
+		listaNomes.add("Chrystine");
+		listaNomes.add("Noob");
+		listaNomes.add("Leon");
+		listaNomes.add("Collin");
+		listaNomes.add("Pisguilo");
+
+
+		for (String nome: listaNomes) {
+			System.out.println(nome);
+
+		}
+
+		
+	}
+
+}
