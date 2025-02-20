bc308b9c56ed30aad5e8794c47470e09e937d4fc
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-19 21:04:37 -0300

    add linkedlist

---
+++ 05-programacao-I/lesson-07/linked-list/Collection.java
@@ -0,0 +1,39 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Collection {
+
+    public static void main(String[] args) {
+
+        //ou LinkedList<String> listaNomes = new LinkedList<>();
+                
+		List<String> listaNomes = new LinkedList<>();
+		listaNomes.add("Chrystine");
+		listaNomes.add("Noob");
+		listaNomes.add("Leon");
+		listaNomes.add("Collin");
+		listaNomes.add("Pisguilo");
+
+		System.out.println("Antes");
+		
+		for (String nome: listaNomes) {
+			System.out.println(nome);
+
+		}
+		
+		listaNomes.set(1, "Frajolinha" );
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Depois");
+
+		for (String nome: listaNomes) {
+			System.out.println(nome);
+
+		}
+		
+	}
+
+}
