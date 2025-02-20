7c8c98e15421f5c16aa041d7eee0bc957ad30cec
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-02-20 15:41:29 -0300

    add ElementoInicio

---
+++ 06-estrutura-de-dados/lesson-08/elementoInicio/ElementoInicio.java
@@ -0,0 +1,14 @@
+public void ElementoInicio () {
+
+    if (!FilaVazia ()) {
+
+        System.out.println("O primeiro elemento é " + vetor[inicio]);
+        
+
+    } else {
+
+        System.out.println("Fila Vazia");
+
+    }
+
+}
