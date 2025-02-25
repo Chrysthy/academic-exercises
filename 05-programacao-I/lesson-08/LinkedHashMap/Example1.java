8e086caebbd2175a74410b3f688dec9123503cd8
Chrystine Martins <chrystine.martins@outlook.com>
2025-02-25 20:58:37 -0300

    add LinkedHashMap

---
+++ 05-programacao-I/lesson-08/LinkedHashMap/Example1.java
@@ -0,0 +1,25 @@
+
+import java.util.LinkedHashMap;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		LinkedHashMap<String, String> listaNomes = new LinkedHashMap<>();
+
+		listaNomes.put("vava", "Oswaldo");
+		listaNomes.put("gisa", "Gisle");
+		listaNomes.put("gigi", "Giovana");
+		listaNomes.put("giugiu", "Giulianna");
+
+		System.out.println("LinkedHashMap " + listaNomes);
+		
+		//creating a LinkedHashMap from other LinkedHashMap 	 
+		LinkedHashMap<String, String> novaListaNomes = new LinkedHashMap<>(listaNomes);
+		novaListaNomes.put("Jubis", "Jujuba");
+		
+		System.out.println("LinkedHashMap2 " + novaListaNomes);
+
+	}
+
+}
