b3dde44a5bfb9467e195fd6d55c8b07972c76f4c
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-01 15:57:25 -0300

    add example execucao

---
+++ 05-programacao-I/lesson-10/excecao/Principal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		Integer dividendo = scanner.nextInt();
+		Integer divisor = scanner .nextInt();
+		
+		
+		try {
+		
+		Integer quociente = dividendo / divisor;
+		System.out.println("Resultado " + quociente);
+		
+		} catch (Exception e) {
+		
+		System.out.println("Causa: " + e.getCause());
+		System.out.println("Mensagem: " + e.getMessage());
+		System.out.println("Tipo de Exceção: " + e.getClass().getCanonicalName());
+		System.out.println("Ops! Não foi possível obter o resultado");
+
+		}	
+		
+		scanner.close();
+		
+	}
+
+}
+
