d128212d7d96f7535a2b9e1bc5e4bf6b3f350d4d
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-01 16:02:29 -0300

    add example 2

---
+++ 05-programacao-I/lesson-10/excecao/Principal2.java
@@ -0,0 +1,20 @@
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			int resultado = 10 / 4;
+			System.out.println(resultado);
+			
+		} catch (Exception e) {
+			
+			System.out.println("Erro " + e.getMessage());
+			
+		}
+
+	}
+
+}
+
