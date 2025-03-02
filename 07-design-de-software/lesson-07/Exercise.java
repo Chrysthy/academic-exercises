05c7daa8388af36f37e8660b01a42fae469c0d11
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-02 13:58:07 -0300

    add exemplo de modificadores

---
+++ 07-design-de-software/lesson-07/Exercise.java
@@ -0,0 +1,16 @@
+public class livro {
+
+	public String titulo;
+
+	protected String autor;
+
+    private int quantidade;
+
+    //Quando o modificador de acesso é omitido, significa que ele é do tipo package (pacote).
+	double preco;
+
+	public String getTitulo() {
+		return null;
+	}
+
+}
