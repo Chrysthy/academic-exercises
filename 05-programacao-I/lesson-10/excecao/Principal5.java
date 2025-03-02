d666474cf4f78e103b549d31f9981e2837d22985
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-02 12:04:49 -0300

    add new example try, catch and finally

---
+++ 05-programacao-I/lesson-10/excecao/Principal5.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Principal5 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("Digite o número 1 ou 2:");
+            System.out.println();
+
+            try {
+
+                opcao = teclado.nextInt();
+
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Você digitou a opção " + opcao);
+                        break;
+
+                    case 2:
+                        System.out.println("Você digitou a opção " + opcao);
+                        break;
+
+                    default:
+                        throw new IllegalArgumentException("Erro: Opção inválida. Digite 1 ou 2.");
+                }
+
+            } catch (IllegalArgumentException excecao) {
+                System.out.println(excecao.getMessage());
+            } catch (Exception e) {
+                System.out.println("Erro inesperado: " + e.getMessage());
+                teclado.nextLine(); // Limpar o buffer do scanner em caso de erro
+            } finally {
+                System.out.println("--------------------------");
+            }
+
+        } while (true);
+      
+    }
+    
+}
