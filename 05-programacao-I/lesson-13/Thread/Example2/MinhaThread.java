18d6ec7c877e95111c9c2b0639e3b2ec63dc70ef
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-03 16:11:29 -0300

    add example2

---
+++ 05-programacao-I/lesson-13/Thread/Example2/MinhaThread.java
@@ -0,0 +1,10 @@
+
+public class MinhaThread extends Thread {
+
+    public void run() {
+
+        System.out.println("Thread Secundária");
+
+    }
+
+}
---
+++ 05-programacao-I/lesson-13/Thread/Example2/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		MinhaThread minhaThread = new MinhaThread();
+		minhaThread.start();
+		
+		System.out.println("Thread Principal");
+
+	}
+
+}
+
