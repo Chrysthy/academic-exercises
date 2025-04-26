83cc265212f7a81591010a246785002d558c7ce7
Chrystine Martins <chrystine.martins@outlook.com>
2025-04-26 15:47:42 -0300

    add criação de tabela 3

---
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/princípios-do-comando-SQL/3-criacao-de-tabela.sql
@@ -0,0 +1,7 @@
+select * from scott.dept;
+-- a tabela dept pertence ao schema scott
+-- o nome da tabela é dept
+-- o oracle live precisa identificar a tabela todo. O nome do usuário ponto e o nome da tabela
+
+select deptno, dname from scott.dept;
+-- especificando as colunas que queremos selecionar, ver.
