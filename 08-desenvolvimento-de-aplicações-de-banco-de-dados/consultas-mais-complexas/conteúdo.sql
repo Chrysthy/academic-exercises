208d0ad9284fa8ca3a9389dc1d900ed892028370
Chrystine Martins <chrystine.martins@outlook.com>
2025-04-27 18:20:58 -0300

    add exemplo modificadores ALL and DISTINCT

---
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-mais-complexas/conteúdo.sql
@@ -0,0 +1,8 @@
+-- *Modificadores ALL e DISTINCT
+
+-- ALL - opção padrão
+-- DISTINCT - mostra somente os valores distintos, repetições não serão mostradas
+
+select distinct job from scott.emp   
+
+
