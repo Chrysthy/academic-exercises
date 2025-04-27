4bd19631414d89b449908048cdf3d44a143a1e20
Chrystine Martins <chrystine.martins@outlook.com>
2025-04-27 18:21:19 -0300

    add Junções naturais e totais (NATURAL e FULL JOINS)

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-mais-complexas/conteúdo.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-mais-complexas/conteúdo.sql
@@ -6,3 +6,21 @@
 select distinct job from scott.emp   
 
 
+-- *Junções naturais e totais (NATURAL e FULL JOINS)
+
+-- Ex. no oracle live
+-- EMP and DEPT (SCOTT)
+-- Busca dados de tabelas diferentes 
+
+--join
+select emp.ename, dept.dname, emp.deptno, dept.deptno
+from scott.emp join scott.dept
+on (emp.deptno = dept.deptno);
+
+--natural join - o oracle faz a junção automaticamente, não é necessário especificar a condição de junção
+-- mas só vale usar a natural join quando as tabelas que fazem parte da junção estiverem relacionadas por uma chave primária e uma chave estrangeira, ou seja, quando houver um relacionamento entre as tabelas.
+select ename, dname, deptno
+from scott.emp natural join scott.dept
+
+
+
