96899e275f03419797dad616110f0e3ff388297c
Chrystine Martins <chrystine.martins@outlook.com>
2025-04-27 18:29:51 -0300

    update file name and content separation

---
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-mais-complexas/all-e-distinct.sql
@@ -0,0 +1,10 @@
+-- *Modificadores ALL e DISTINCT
+
+-- ALL - opção padrão
+-- DISTINCT - mostra somente os valores distintos, repetições não serão mostradas
+
+select distinct job from scott.emp   
+
+
+-- Ex. no oracle live
+-- EMP and DEPT (SCOTT)
--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-mais-complexas/join-naturalJoin.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-mais-complexas/join-naturalJoin.sql
@@ -1,11 +1,3 @@
--- *Modificadores ALL e DISTINCT
-
--- ALL - opção padrão
--- DISTINCT - mostra somente os valores distintos, repetições não serão mostradas
-
-select distinct job from scott.emp   
-
-
 -- *Junções naturais e totais (NATURAL e FULL JOINS)
 
 -- Ex. no oracle live
