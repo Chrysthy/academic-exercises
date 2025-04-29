c0d532eb5b6abd262626806721ff66cb6a861350
Chrystine Martins <chrystine.martins@outlook.com>
2025-04-29 16:44:24 -0300

    add previsão de dados

---
+++ 09-prática-integradora-tecnologias-dsruptivas/pratica-de-bi-com-python-basico/aula2.py
@@ -0,0 +1,13 @@
+#previsão de dados
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+saude_df = pd.read_csv('/content/BI_saude_dados_simulados.csv', delimiter=';')
+
+X = saude_df[['Impressões', 'CTR', 'Lat', 'Long']]
+y = saude_df['Custo_por_Conversao']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
