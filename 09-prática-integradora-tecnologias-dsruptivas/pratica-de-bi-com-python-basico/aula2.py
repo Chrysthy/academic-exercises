24763d085465419a1e923979b9080e4f91f21049
Chrystine Martins <chrystine.martins@outlook.com>
2025-04-29 17:53:52 -0300

    add criando o modelo

--- 09-prática-integradora-tecnologias-dsruptivas/pratica-de-bi-com-python-basico/aula2.py
+++ 09-prática-integradora-tecnologias-dsruptivas/pratica-de-bi-com-python-basico/aula2.py
@@ -11,3 +11,12 @@ X = saude_df[['Impressões', 'CTR', 'Lat', 'Long']]
 y = saude_df['Custo_por_Conversao']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+#criando o modelo
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+
+print(mse)
