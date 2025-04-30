c73f45e1ce65467774bd9c9700241dbd5e66bc17
Chrystine Martins <chrystine.martins@outlook.com>
2025-04-30 13:29:43 -0300

    add example 2

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-com-agrupamento-totalizacao-e-ordenacao/subconsultas.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/consultas-com-agrupamento-totalizacao-e-ordenacao/subconsultas.sql
@@ -7,10 +7,19 @@
 
 --Iremos fazer primeiro a consulta de dentro, pra descobror qual é o departamento do Smith e essa consulta de dentro irá fornecer o valor para a consulta de fora. Onde iremos buscar o nome e o número do departamento de quem trabalha no mesmo departamento do Smith.
 
+--Retorno da Subconsulta
+-- Saber o departamento
+select deptno 
+from scott.emp
+where ename = 'SMITH';
+
 -- Consulta de dentro/subconsulta:
 -- saber o departamento do Smith
 --sempre entre parênteses
 --colocar a subconsulta mais a direta e a consulta de fora mais a esquerda
+
+
+-- Consulta de fora:
 select ename, deptno
 from scott.emp
 where deptno = (select deptno
@@ -19,8 +28,22 @@ where deptno = (select deptno
 
 
 
--- Saber o departamento
-select deptno 
+
+
+-- Exemplo 2:
+-- Exiba o nome  e o cargo de quem ganha igual ou mais que Scott.
+
+--Retorno da Subconsulta
+-- Descobrir quanto ganha o Scott
+select sal 
 from scott.emp
-where ename = 'SMITH';
+where ename = 'SCOTT';
+
+
+-- Consulta de de fora:
+select ename, job
+from scott.emp
+where sal >= (select sal
+                from scott.emp
+                where ename = 'SCOTT');
 
