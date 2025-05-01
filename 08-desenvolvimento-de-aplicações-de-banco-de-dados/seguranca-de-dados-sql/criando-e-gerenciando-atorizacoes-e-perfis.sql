d72463ba990d527175f74a5a1de66c03be5f5683
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-01 15:49:54 -0300

    add bloqueando e desbloqueando usuário

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/seguranca-de-dados-sql/criando-e-gerenciando-atorizacoes-e-perfis.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/seguranca-de-dados-sql/criando-e-gerenciando-atorizacoes-e-perfis.sql
@@ -10,10 +10,31 @@ create user peter
 identified by peter
 default tablespace system
 quota 15m on system;
+--está relacionada a limitar o espaço em disco (quota) que um usuário pode usar dentro de uma tablespace (neste caso, a SYSTEM). 15 megabytes.
 
 grant create session to peter;
 
 conn peter/peter;
 
+show user; -- Verifica se o usuário foi criado com sucesso
+
+--Dando um select na dba users para ver o status do usuário
+select username, account_status from dba_users order by username;
 
 
+-- Bloqueando o usuário
+conn system/manager
+
+alter user peter account lock; -- Bloqueia o usuário, ele não consegue mais logar no banco de dados.
+
+conn peter/peter; -- Tenta logar com o usuário bloqueado, não vai conseguir.
+-- O status do usuário vai ficar como LOCKED (bloqueado) e não vai conseguir logar no banco de dados.
+
+
+-- Desbloqueando o usuário
+conn system/manager
+
+select username, account_status, lock_date, expiry_Date, default_tablespace, profile from dba_users where username = 'PETER'; -- Verifica o status do usuário antes de desbloquear
+
+alter user peter
+account unlock; -- Desbloqueia o usuário, ele consegue logar novamente no banco de dados.
