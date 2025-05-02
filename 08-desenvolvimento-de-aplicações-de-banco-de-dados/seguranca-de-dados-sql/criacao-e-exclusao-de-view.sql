a7f02c7493596129ecbb88a6036e361358b9fc4a
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-02 09:26:50 -0300

    add eliminar a view

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/seguranca-de-dados-sql/criacao-e-exclusao-de-view.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/seguranca-de-dados-sql/criacao-e-exclusao-de-view.sql
@@ -12,3 +12,11 @@ SELECT * FROM salvu31;
 
 
 
+-- Para eliminar uma view, o comando é DROP VIEW, seguido do nome da view a ser eliminada.
+DROP VIEW empvu10;
+
+-- Para verificar se a view foi eliminada.
+select * from empvu10;
+
+-- O fato de eliminar a view não elimina a tabela base, ou seja, a tabela EMP continua existindo.
+select * from emp;
