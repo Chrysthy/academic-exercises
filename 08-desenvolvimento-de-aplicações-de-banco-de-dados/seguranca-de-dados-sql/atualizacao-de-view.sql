2bf145cfe08581a8278fa9fc3b838c2b8eb3e2e2
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-02 09:43:29 -0300

    add recriar view whit read only

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/seguranca-de-dados-sql/atualizacao-de-view.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/seguranca-de-dados-sql/atualizacao-de-view.sql
@@ -22,3 +22,18 @@ select * from dept_sum_vu_10;
 
 
 
+-- Para recriar a view
+CREATE OR REPLACE VIEW empvu_10 (empno, ename, job)
+AS SELECT empno, ename, job
+FROM emp
+WHERE deptno = 10
+WITH READ ONLY; -- A view é somente leitura. Não poderá ocorrer operações DML (Data Manipulation Language) como INSERT, UPDATE ou DELETE.
+
+select * from empvu_10; -- Para acessar os dados
+
+-- Tentar fazer um update na view
+update empvu_10 
+set ename = 'JOSÉ' 
+where empno = 7839; -- Não é possível fazer update na view, pois ela é somente leitura. Da erro.
+
+select * from empvu_10;
