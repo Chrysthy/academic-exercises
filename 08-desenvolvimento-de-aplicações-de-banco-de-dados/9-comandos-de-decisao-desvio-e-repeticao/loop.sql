334cc8cdcf29dbb62f4eda8d979265f98a5b1789
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-03 11:32:00 -0300

    add example 2

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/9-comandos-de-decisao-desvio-e-repeticao/loop.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/9-comandos-de-decisao-desvio-e-repeticao/loop.sql
@@ -25,3 +25,28 @@ BEGIN
        DBMS_OUTPUT.PUT_LINE('Fatorial de ' || N || ' é: ' || FATORIAL);
 END;
 / 
+
+
+--Examplo 2
+-- Criando uma tabela de exemplo para o loop
+conn system/manager
+
+create table teste_loop (
+    id number, nome varchar2(50)
+);
+
+DECLARE
+    v_counter number := 1;
+    v_nome varchar2(10):= 'Teste Loop';
+BEGIN
+    LOOP
+        INSERT INTO teste_loop (id, nome) VALUES (v_counter, v_nome);
+        v_counter := v_counter + 1;
+        
+    EXIT WHEN v_counter > 5; -- Condição de saída do loop
+
+    END LOOP;
+END;
+/
+
+select * from teste_loop;
