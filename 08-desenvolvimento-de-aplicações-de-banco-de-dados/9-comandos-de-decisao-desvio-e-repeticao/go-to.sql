ceecf0e1f7d8a34712e10798fcc50dfc7b1ef86d
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-03 10:55:02 -0300

    add example 2

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/9-comandos-de-decisao-desvio-e-repeticao/go-to.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/9-comandos-de-decisao-desvio-e-repeticao/go-to.sql
@@ -23,3 +23,26 @@ BEGIN
        DBMS_OUTPUT.PUT_LINE('Fatorial de ' || n || ' é: ' || fatorial);
 END;
 /
+
+
+-- Example 2
+DECLARE 
+    FATORIAL NUMBER := 1;
+    N NUMBER;
+    I NUMBER := 0;
+BEGIN
+    N := 5;
+    I := N;
+
+    <<inicio_loop>>
+
+    IF I >= 1 THEN
+        FATORIAL := FATORIAL * I;
+        I := I - 1;
+        GOTO inicio_loop; -- Desvio de fluxo
+    END IF;
+       DBMS_OUTPUT.PUT_LINE('Fatorial de ' || N || ' é: ' || FATORIAL);
+END;
+/
+
+-- Fatorial é 5 != 5 * 4 * 3 * 2 * 1 = 120
