ac5e0591f6263d635130f95cb2305441d20195f0
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-13 20:14:47 -0300

    add Perfil do usuário

--- 09-prática-integradora-tecnologias-dsruptivas/4-desenvolvimento-de-solução-de-machine-learning/recomendacoes.py
+++ 09-prática-integradora-tecnologias-dsruptivas/4-desenvolvimento-de-solução-de-machine-learning/recomendacoes.py
@@ -10,3 +10,10 @@ def recomendar_filmes(usuario_id, interacoes_usuario, filmes, num_recomendacoes=
     filmes_assistidos = interacoes_usuario_especifico['filme_id'].values
     filmes_curtidos = interacoes_usuario_especifico[interacoes_usuario_especifico['interacao'] > 0]['filme_id'].values
     caracteristicas_filmes_curtidos = filmes[filmes['filme_id'].isin(filmes_curtidos)].iloc[:, 2:].values
+
+    # Perfil do usuário
+    perfil_usuario = np.mean(caracteristicas_filmes_curtidos, axis=0).reshape(1, -1)
+    filmes_nao_assistidos = filmes[~filmes['filme_id'].isin(filmes_assistidos)]
+    similaridades = cosine_similarity(perfil_usuario, filmes_nao_assistidos.iloc[:, 2:].values).flatten()
+
+    
