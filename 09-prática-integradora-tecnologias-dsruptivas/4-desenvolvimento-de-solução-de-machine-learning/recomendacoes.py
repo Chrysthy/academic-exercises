5850a66e3bf7bc5d0f1fc5e53a1ef6701c6e964d
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-13 20:40:53 -0300

    add geração de dados

--- 09-prática-integradora-tecnologias-dsruptivas/4-desenvolvimento-de-solução-de-machine-learning/recomendacoes.py
+++ 09-prática-integradora-tecnologias-dsruptivas/4-desenvolvimento-de-solução-de-machine-learning/recomendacoes.py
@@ -22,3 +22,38 @@ def recomendar_filmes(usuario_id, interacoes_usuario, filmes, num_recomendacoes=
 
     return filmes_recomendados, filmes_curtidos, perfil_usuario 
 
+    categorias = ['Ação', 'Drama', 'Comédia', 'Romance', 'Suspense', 'Ficção Científica', 'Fantasia', 'Histórico', 'Terror', 'Documentário']
+
+    temas = ['Mistério', 'Amor proibido', 'Criaturas místicas', 'Biografias', 'Horror psicológico', 'Superação', 'Inteligência artificial', 'Triângulo amoroso', 'Sobrevivência']
+
+    num_filmes = 1000
+    num_usuarios = 100
+
+
+    filmes  = pd.DataFrame({
+        'filme_id': range(1, num_filmes + 1),
+        'titulo': [f'Filme {i}' for i in range(1, num_filmes + 1)],
+        **{f'categoria_{categoria.lower()}': np.random.randint(0, 2, num_filmes) for categoria in categorias},
+        **{f'tema_{tema.lower().replace(" ", "_")}': np.random.randint(0, 2, num_filmes) for tema in temas}
+    })
+    
+
+    # Gerando interações do usuário
+    interacoes_usuario = pd.DataFrame({
+        'usuario_id': np.random.randint(1, num_usuarios + 1, num_filmes * 10),
+        'filme_id': np.random.randint(1, num_filmes + 1, num_filmes * 10),
+        'interacao': np.random.randint(0, 2, num_filmes * 10)
+    })
+
+    def gerar_interacoes(num_usuarios, num_filmes, interacoes_por_usuario):
+        interacoes_usuarios = []
+    for usuario_id in range(1, num_usuarios + 1):
+        filmes_ids = np.random.choice(filmes['filme_id'], interacoes_por_usuario, replace=False)
+    interacoes = np.random.choice([1,2,-1], interacoes_por_usuario, replace=True)
+    for filme_id, interacao in zip(filmes_ids, interacoes):
+            interacoes_usuarios.append({
+                'usuario_id': usuario_id, 
+                'filme_id': filme_id, 
+                'interacao': interacao
+            })
+
