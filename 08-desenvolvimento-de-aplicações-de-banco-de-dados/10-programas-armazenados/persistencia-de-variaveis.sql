05eb35dd689eadd7e2c7edace47eaa6d40db52db
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-19 14:27:34 -0300

    update content

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/10-programas-armazenados/persistencia-de-variaveis.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/10-programas-armazenados/persistencia-de-variaveis.sql
@@ -27,3 +27,35 @@ BEGIN
             COMMIT;
             chamadas_reajuste:= chamadas_reajuste+1;
 END reajuste;
+
+
+FUNCTION descobrir_salario
+(p_id in emp.Empno%type)
+return number
+IS 
+    v_salario emp.sal%type := 0;
+    BEGIN
+chamadas_descobrir_salario:= chamadas_descobrir_salario+1
+
+Select sal into v_salario
+From emp
+Where  empno = p_id;
+Return v_salario;
+End descobrir_salario;
+
+End exemplo_pack;
+/
+
+select exemplo_pack.descobrir_salario(7934) from dual;
+execute exemplo_pack.reajuste(7934,25);
+
+select exemplo_pack.descobrir_salario(7934) from dual;
+
+
+SET SERVEROUTPUT ON;
+BEGIN   
+
+        DBMS_OUTPUT.PUT_LINE(EXEMPLO_PACK.chamadas_reajuste);
+        DBMS_OUTPUT.PUT_LINE(EXEMPLO_PACK.chamadas_descobrir_salario);
+END;
+/
