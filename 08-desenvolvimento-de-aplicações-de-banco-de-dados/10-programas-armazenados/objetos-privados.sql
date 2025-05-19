b3dae14ec2393796a76f3f4b86ca91194de6b2b6
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-19 16:00:01 -0300

    add Objetos privados

---
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/10-programas-armazenados/objetos-privados.sql
@@ -0,0 +1,31 @@
+-- É um são declarados no corpo do pacote, chamadas de variáveis locais.
+
+CREATE OR REPLACE PACKAGE pkg_string_func AS 
+    FUNCTION get_cont_localiza
+    RETURN NUMBER;
+END pkg_string_func;
+/
+
+CREATE OR REPLACE PACKAGE BODY pkg_string_func AS 
+    -- Esta variável é local
+    chamadas_localiza NUMBER := 0;
+
+    FUNCTION get_cont_localiza
+    RETURN  
+        NUMBER
+    IS
+    BEGIN
+        chamadas_localiza:=chamadas_localiza+1;
+        RETURN chamadas_localiza;
+        END;
+END pkg_string_func;
+/
+
+SET SERVEROUTPUT ON;
+declare
+v_recebe number;
+BEGIN   
+    select (pkg_string_func.get_cont_localiza) into v_recebe from dual;
+    dbms_output.put_line('O valor da variável local é: ' || v_recebe);
+END;
+/
