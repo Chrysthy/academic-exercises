3918685252fd8a0e8fbbfc3d5e23b5065454b016
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-22 13:44:45 -0300

    add example 2

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/12-triggers-dml/pseudoregistros-old-new.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/12-triggers-dml/pseudoregistros-old-new.sql
@@ -28,3 +28,22 @@ where empno=7788;
 select job from emp where empno=7788; 
 
 alter session set nls_date_format - 'DY HH24:MI';
+
+-- Example 2
+
+CREATE OR REPLACE TRIGGER seguro_emp
+BEFORE Insert ON SCOTT.emp
+BEGIN   
+    if(TO_CHAR(SYSDATE, 'DY') IN ('SAB', 'DOM')) OR (TO_CHAR(SYSDATE, 'HH24:MI')
+    BETWEEN '08:00' AND '17:00') Then
+RAISE_APPLICATION_ERROR(-20500, 'Inscrições de funcionários somente dora do horário comercial');
+    end if;
+    end;
+
+/
+
+--para ver que dia e horas
+select TO_CHAR(SYSDATE, 'dd/mm/yyyy-hh24:mi:ss') from dual;
+
+
+insert into SCOTT.emp(empno, ename, sal, deptno) values (7935, 'SANDRA', 1203, 10);; -- ativa a trigger
