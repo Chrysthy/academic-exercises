6cb2ad1953d8a3e59e8da80895c7a58b77192d40
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-26 13:42:36 -0300

    add example 2

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/13-cursores-implicitos-explicitos/cursores-introducao.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/13-cursores-implicitos-explicitos/cursores-introducao.sql
@@ -1,4 +1,4 @@
--- Example 1
+-- Example 1 - cursor implícito
 
 conn scott/tiger
 
@@ -18,3 +18,33 @@ END;
 
 print linhas_deletadas
 rollback;
+
+
+-- Example 2 - cursor explícito
+
+ACCEPT p_cargo_func PROMPT 'Digite o CARGO do funcionário '
+VARIABLE g_n_ocorrencias NUMBER;
+drop table sal_tot;
+create table sal_tot (ename VARCHAR2(50), sal_mes number (7,2), sal_tot number (7,2));
+
+DECLARE 
+    v_cargo_func emp.job%TYPE := upper ('&p_cargo_func')/
+    v_nome_func emp.ename%TYPE;
+    v_sal_mes emp.sal%TYPE;
+    v_sal_ano number(9,2);
+
+        CURSOR sal_func_cursor IS
+                SELECT ename, sal, sal * 12
+                FROM emp
+                WHERE job = v_cargo_func;
+BEGIN
+OPEN sal_func_cursor;
+            LOOP
+FETCH sal_func_cursor INTO v_nome_func, v_sal_mes, v_sal_ano;
+EXIT WHEN sal_func_cursor%NOTFOUND;
+INSERT INTO sal_tot(ename, sal_mes, sal_tot) VALUES (v_nome_func, v_sal_mes, v_sal_ano);
+END LOOP;
+    :g_n_ocorrencias := sal_func_cursor%ROWCOUNT;
+    CLOSE sal_func_cursor;
+END;
+/
