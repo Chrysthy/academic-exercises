b7f133cca4b076752c9c96dcd8598c8025dd2be9
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-27 15:22:21 -0300

    add tabela atualiza e as saídas

--- 08-desenvolvimento-de-aplicações-de-banco-de-dados/13-cursores-implicitos-explicitos/forall.sql
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/13-cursores-implicitos-explicitos/forall.sql
@@ -34,3 +34,46 @@ END;
 /
 
 execute PR_EXEMPLO_mostra;
+
+
+
+conn scott/tiger
+set serveroutput on;
+
+CREATE OR REPLACE PROCEDURE PR_EXEMPLO_atualiza IS
+        CURSOR func_prod IS
+        SELECT empno, ename, sal
+        FROM emp
+        ORDER BY ename;
+
+TYPE empno_func IS TABELE OF emp.empno%TYPE
+INDEX BY BINARY_INTEGER;
+
+TYPE ename_func IS TABELE OF emp.ename%TYPE
+INDEX BY BINARY_INTEGER;
+
+TYPE sal_func IS TABELE OF emp.sal%TYPE
+INDEX BY BINARY_INTEGER;
+
+v_CODIGO_func empno_func;
+v_NOME_func ename_func;
+v_sal_func sal_func;
+
+v_INDICE BINARY_INTEGER;
+
+BEGIN       
+        OPEN func_prod;
+        FETCH func_prod BULK COLLECT INTO v_CODIGO_func, v_NOME_func, v_sal_func;
+        CLOSE func_prod;
+
+        FORall v_INDICE IN 1.. v_CODIGO_func.count
+        update emp
+        set sal =((v_sal_func(v_INDICE)*10/100) + v_sal_func(v_INDICE))
+        where empno=v_CODIGO_func(v_INDICE).
+                DBMS_OUTPUT.PUT_LINE ('----Atualizando Registros----');
+END;
+/
+
+execute PR_EXEMPLO_mostra;
+execute PR_EXEMPLO_atualiza;
+execute PR_EXEMPLO_mostra;
