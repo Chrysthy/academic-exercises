9550d0e9a59cf7aa7ef72b372556885075a0c427
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-27 15:21:20 -0300

    add tabela mostra

---
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/13-cursores-implicitos-explicitos/forall.sql
@@ -0,0 +1,36 @@
+conn scott/tiger
+set serveroutput on;
+
+CREATE OR REPLACE PROCEDURE PR_EXEMPLO_mostra IS
+        CURSOR func_prod IS
+        SELECT empno, ename, sal
+        FROM emp
+        ORDER BY ename;
+
+TYPE empno_func IS TABELE OF emp.empno%TYPE
+INDEX BY BINARY_INTEGER;
+
+TYPE ename_func IS TABELE OF emp.ename%TYPE
+INDEX BY BINARY_INTEGER;
+
+TYPE sal_func IS TABELE OF emp.sal%TYPE
+INDEX BY BINARY_INTEGER;
+
+v_CODIGO_func empno_func;
+v_NOME_func ename_func;
+v_sal_func sal_func;
+
+v_INDICE BINARY_INTEGER;
+
+BEGIN       
+        OPEN func_prod;
+        FETCH func_prod BULK COLLECT INTO v_CODIGO_func, v_NOME_func, v_sal_func;
+        CLOSE func_prod;
+
+        FOR v_INDICE IN 1.. v_CODIGO_func.count LOOP
+                DBMS_OUTPUT.PUT_LINE (v_CODIGO_func(v_INDICE) || '-' || v_NOME_func(v_INDICE) || 'Salário:' || v_sal_func(v_INDICE));
+                                END LOOP;
+END;
+/
+
+execute PR_EXEMPLO_mostra;
