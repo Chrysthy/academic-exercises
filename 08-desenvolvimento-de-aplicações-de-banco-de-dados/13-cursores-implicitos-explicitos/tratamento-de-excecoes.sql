b222121943f0ddbb0d6b0c882b69e4732558f6ec
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-28 13:17:45 -0300

    add explicação exceptions

---
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/13-cursores-implicitos-explicitos/tratamento-de-excecoes.sql
@@ -0,0 +1,5 @@
+-- Em SQL, exceptions (exceções) são mecanismos usados principalmente em procedimentos armazenados (stored procedures), gatilhos (triggers) ou blocos 
+-- de código PL/SQL ou T-SQL para capturar e tratar erros que ocorrem durante a execução de comandos SQL.
+
+-- Elas funcionam de forma semelhante às exceções em linguagens de programação como Python, Java ou C#, permitindo 
+-- identificar um erro, interromper o fluxo normal da execução e executar um bloco de código para lidar com esse erro.
