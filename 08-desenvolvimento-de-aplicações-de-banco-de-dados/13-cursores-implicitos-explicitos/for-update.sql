593a03fb438f1fbe41b24f906f87d0d88a3c67a3
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-30 11:28:29 -0300

    add example for update

---
+++ 08-desenvolvimento-de-aplicações-de-banco-de-dados/13-cursores-implicitos-explicitos/for-update.sql
@@ -0,0 +1,29 @@
+
+select JOB, SAL from emp;
+
+DECLARE
+
+cursor cursor_ex_atual is
+    select ename, job, sal from emp
+        for update of sal nowait.
+
+reg_ex_atual cursor_ex_atual%rowtype;
+
+BEGIN
+
+    for reg_ex_atual in cursor_ex_atual loop
+        if reg_ex_atual.job = 'CLERK' then
+            update emp set sal = sal * 1.10
+            where current of cursor_ex_atual;
+        elseif reg_ex_atual.job = 'PRESIDENT' then
+            update emp set sal = sal + sal
+            where current of cursor_ex_atual;
+        else 
+            delet from emp
+            where current of cursor_ex_atual;
+        end if;
+    end loop;
+end;
+/
+
+select JOB, SAL from emp;
