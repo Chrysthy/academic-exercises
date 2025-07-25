2ce73994417516875fa3e965557433297243aea9
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-25 20:50:42 -0300

    create server

---
+++ 11-programacao-ii/lesson-1/hello.js
@@ -0,0 +1,15 @@
+import { createServer } from 'http';
+
+const hostName = '12.0.0.1'
+const port = 3000;
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello World!');
+    res.end();
+})
+
+//localhost:3000
+server.listen(port, hostName, () => {
+    console.log(`Server running at http://${hostName}:${port}/`);
+});
